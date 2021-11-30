ecdb3c8e43ed5ef2b61f0f2a3d4d806f7312c791
pauloschade <paulosc4@al.insper.edu.br>
2021-11-30 20:05:34 -0300

    simulado PF

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Round from './Round'
 import Header from './Header'
+import axios from 'axios'
 
 export default function Main() {
+
+  const [resp, setResp] = useState("")
+  useEffect(() => {
+    axios.get("https://enigmatic-bayou-56424.herokuapp.com/paulosc4/token",{})
+    .then(response => {
+      axios.post("https://enigmatic-bayou-56424.herokuapp.com/paulosc4/message",{
+        "token" :response.data.token
+      })
+      .then((response) => {
+        setResp(response.data.mensagem)
+      })
+    },)
+  })
   return(
     <div>
       <Header/>
       <Round />
+      <div>{resp}</div>
     </div>
   )
 }
